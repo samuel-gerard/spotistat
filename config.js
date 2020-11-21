aa3d309c0ad4f8048a9d3bb2a126021622663db8
Samuel Gérard <samuel.gerard98@gmail.com>
2020-11-21 22:49:57 +0100

    Change redirect uri

--- config.js
+++ config.js
@@ -1,6 +1,7 @@
 const client_id = 'd8a5a397a7b8470e9beb7d2238ac3197' // Your client id
 const client_secret = '00a37ee481a547ed888ad593c11af535'; // Your secret
-const redirect_uri = 'http://localhost:8888/callback/'; // Your redirect uri
+const redirect_uri = 'https://spotistat.herokuapp.com/callback/'; // Your redirect uri
+// const redirect_uri = 'http://localhost:8888/callback/'; // Your redirect uri
 
 module.exports = {
     client_id,
